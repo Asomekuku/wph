ce5e548c07c3c28bce167f06d5b907ef986427b2
KBMonster <1940124356@qq.com>
2020-09-23 11:39:54 +0800

    购物车

--- src/routes/Cart/Cart.js
+++ src/routes/Cart/Cart.js
@@ -1,48 +1,66 @@
 import React from 'react'
 import './cart.scss'
 
+//引入这个高阶函数
+import { connect } from "react-redux";
+import { goodAdd,goodSub,getGoods,goodDel,time } from "../../store/actions/goodAction";
+
 import { NavBar, Icon } from 'antd-mobile';
-import { axiosGoodDetails } from '@/utils/api'
 
-import { KBSecure,KBLocation } from '@/components/'
+import { KBSecure,KBLocation,NoGoods } from '@/components/'
 
-export default class Cart extends React.Component{
+function mapStateToProps(store) {
+    return {
+        goods:store.good.goods,
+        minute:store.good.minute,
+        second:store.good.second
+    };
+  }
+  //把action生成器方法，映射到props上面
+  function mapActionToProps(dispatch) {
+    return {
+        getGoods:(params)=>dispatch(getGoods(params)),
+        goodAdd:(idx)=>dispatch(goodAdd(idx)),
+        goodSub:(idx)=>dispatch(goodSub(idx)),
+        goodDel:(idx)=>dispatch(goodDel(idx)),
+        time:()=>dispatch(time())
+    };
+  }
+
+class Cart extends React.Component{
     constructor(props){
         super(props)
         this.state={
-            goods:[]
+            user:false, //判断用户登录了没
         }
     }
-    async componentDidMount(){
-        localStorage.setItem('token','123456')
-        let arr = await axiosGoodDetails({
-            apikey:'e66p5gkSEl3lmPJ4bV28mbUe8I78ewhX',
-            id:'6918181929819039310'
-        })
-        // console.log(arr.data.data)
-        let data = {
-            img:arr.data.data.goodsMainPicture,
-            shop_title:arr.data.data.storeInfo,
-            title:arr.data.data.goodsName,
-            market_price:arr.data.data.marketPrice,
-            vip_price:arr.data.data.vipPrice
-            
-        }
-        localStorage.setItem('item',JSON.stringify(data))
-        this.setState({goods:[JSON.parse(localStorage.getItem('item'))]})
-        
+    componentDidMount(){
         if(localStorage.getItem('token')){
             console.log('已登录')
+            this.setState({user:true})      //打开开关
+
+            //获取假数据
+            this.props.getGoods({
+                apikey:'e66p5gkSEl3lmPJ4bV28mbUe8I78ewhX',
+                id:'6918181929819039310'
+            })
         }else{
             console.log('未登入')
         }
+
+        setInterval(()=>{
+            this.props.time()
+            // console.log(`多少分${this.props.minute}多少秒${this.props.second}`)
+        },1000)
+        this.setState({isTime:true})
         
         
+        
+        // let date_over = date
     }
     createShopCart(){
-        let { goods } =this.state
-        console.log(goods)
-        return this.state.goods.map((ele,idx)=>(
+        // console.log(this.props.goods)
+        return this.props.goods.map((ele,idx)=>(
             <div key={idx} className="cart_good">
                 <div>
                     <img src={ele.img} alt=""/>
@@ -54,14 +72,14 @@ export default class Cart extends React.Component{
                         <em>7天可退</em><em>退换无忧</em>
                     </li>
                     <li>
-                        <span><i className="iconfont icon-jian"></i></span>
-                        <span><input onChange={()=>{console.log('change')}} value="1" type="text"/></span>
-                        <span><i className="iconfont icon-jia"></i></span>
+                        <span onClick={this.dian.bind(this,false,idx) }><i className="iconfont icon-jian"></i></span>
+                        <span><input onChange={()=>{console.log('change')}} value={ele.good_num} type="text"/></span>
+                        <span onClick={this.dian.bind(this,true,idx)}><i className="iconfont icon-jia"></i></span>
                     </li>
                 </ul>
                 <div>
                     <span>￥{ele.vip_price}</span>
-                    <div>
+                    <div onClick={()=>{this.props.goodDel(idx)}}>
                         <i className="iconfont icon-cuo"></i>
                     </div>
                 </div>
@@ -69,40 +87,75 @@ export default class Cart extends React.Component{
             </div>
         ))
     }
+    jump(){
+        this.props.history.push('/login/'+123)
+    }
+    count(){
+        let num=0
+        this.props.goods.map(ele=>{
+            num+=parseInt(ele.vip_price)*ele.good_num
+            return null
+        })
+        return num
+    }
+    dian(bol,idx){
+        if(bol){
+            this.props.goodAdd(idx)
+        }else{
+            this.props.goodSub(idx)
+        }
+    }
     render(){
-        let { goods } =this.state
-        console.log(goods)
+        let { user } =this.state
+        let { goods } = this.props
+        // console.log('状态管理goods',goods)
+        
         return (
             <div className="cart">
-                <NavBar
-                    mode="light"
-                    icon={<Icon type="left" />}
-                    onLeftClick={() => console.log('onLeftClick')}
-                >确认订单</NavBar> 
-                <KBSecure></KBSecure>
-                <KBLocation></KBLocation>
-                <div className="goods_list">
-                    <h1>唯品自营</h1>
-                    {this.createShopCart()}
-                </div>
-                <div className="goods_price">￥116</div>
-                <div className="yhq">
-                    <span>使用优惠券</span>
-                    <Icon type="right" />
-                </div>
-                <div>
-                    <span>订单金额</span>
-                    <em>￥178</em>
-                </div>
-                <div>
-                    <span>商品总金额</span>
-                    <em>￥178</em>
-                </div>
-                <div>
-                    <span>还需支付：<em>￥178</em></span>
-                    <div>在线支付</div>
-                </div>
+                {/* 三目运算符判断是否登入成功来显示对应的页面 */}
+
+                {goods.length
+                ?   (<>
+                        <NavBar
+                            mode="light"
+                            icon={<Icon type="left" />}
+                            onLeftClick={() => this.props.history.go(-1)}
+                            >确认订单{`还剩${this.props.minute}分${this.props.second}`}</NavBar> 
+                        <KBSecure></KBSecure>
+                        <KBLocation></KBLocation>
+                        <div className="goods_list">
+                            <h1>唯品自营</h1>
+                            {this.createShopCart()}
+                        </div>
+                        <div className="goods_price">￥{this.count()}</div>
+                        <div className="yhq">
+                            <span>使用优惠券</span>
+                            <Icon type="right" />
+                        </div>
+                        <div>
+                            <span>订单金额</span>
+                            <em>￥{this.count()}</em>
+                        </div>
+                        <div>
+                            <span>商品总金额</span>
+                            <em>￥{this.count()}</em>
+                        </div>
+                        <div>
+                            <span>还需支付：<em>￥{this.count()}</em></span>
+                            <div>在线支付</div>
+                        </div>
+                    </>)
+                :   ( user
+                    ?   (   <NoGoods></NoGoods>)
+                    :   (   <div className="no-login">
+                                <h1>未登录</h1>
+                                <button onClick={this.jump.bind(this)}>去登录</button>
+                            </div>)
+                    )
+                }
             </div>
         )
     }
 }
+
+export default connect(mapStateToProps, mapActionToProps)(Cart);
---
+++ src/utils/img.js
@@ -0,0 +1,10 @@
+// import cart_bg from '@/public/cart_bg.png'
+
+let ImgBaseUrl = 'http://localhost:8080/'
+
+
+
+export default{
+    cart_bg:ImgBaseUrl+'cart_bg.png',
+    cart:ImgBaseUrl+'cart.png'
+}
